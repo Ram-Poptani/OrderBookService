4aa2659266740d058db316ecf05ee1619fe16486
Ram Poptani <911S205126@adani.com>
2026-04-16 04:00:09 +0530

    chore: Add OrderBookSnapshot model to represent order book data

---
+++ src/main/java/org/binance/orderbookservice/model/OrderBookSnapshot.java
@@ -0,0 +1,29 @@
+package org.binance.orderbookservice.model;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+@Builder
+public class OrderBookSnapshot {
+
+    private BigDecimal bestBidPrice;
+
+    private BigDecimal bestBidAmount;
+
+    private BigDecimal bestAskPrice;
+
+    private BigDecimal bestAskAmount;
+
+    private BigDecimal spread;
+
+    private Integer bidLevels;
+
+    private Integer askLevels;
+
+    private Long timestamp;
+}
