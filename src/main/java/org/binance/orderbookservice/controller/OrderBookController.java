a8a8b4aae5c62f5ca51f9d73c20782722fbb31f5
Ram Poptani <911S205126@adani.com>
2026-05-12 06:14:01 +0530

    fix: Fix OrderBookController return type and input validation

--- src/main/java/org/binance/orderbookservice/controller/OrderBookController.java
+++ src/main/java/org/binance/orderbookservice/controller/OrderBookController.java
@@ -1,4 +1,26 @@
 package org.binance.orderbookservice.controller;
 
+
+import org.binance.orderbookservice.engine.OrderBook;
+import org.binance.orderbookservice.model.OrderBookView;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class OrderBookController {
+
+    private final OrderBook orderBook;
+
+    OrderBookController(OrderBook OrderBook) {
+        this.orderBook = OrderBook;
+    }
+
+    @GetMapping("/orderbook")
+    public OrderBookView getOrderBook(
+            @RequestParam(defaultValue = "10") int levels
+    ) {
+        if (levels < 0 ) { levels = 0; }
+        return orderBook.getView(levels);
+    }
 }
